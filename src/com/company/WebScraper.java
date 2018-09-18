d2128f41a818af2746d2cefcfc90a6d6ecdccf55
sangh <sshin51@illinois.edu>
2018-09-18 16:50:22 -0500

    tryy 1

--- src/com/company/WebScraper.java
+++ src/com/company/WebScraper.java
@@ -1,5 +1,41 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
-    public static void main(String[] unused) {
-        System.out.println("HERE");
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
+
+    public static void main(String[] unused) {
+        String script = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        //System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+
+        wordCount(script);
+    }
+
+    String script = urlToString("http://erdani.com/tdpl/hamlet.txt");
+
+    public static int wordCount(String script) {
+        String[] count = script.split(" ");
+        
+        System.out.println(count.length);
+        return count.length;
+    }
+
 }
